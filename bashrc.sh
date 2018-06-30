5ae5bc9b321d712d723aafd42b38308e90fd7c86
Roberto <robertozz@users.noreply.github.com>
2018-06-30 05:05:12 +0200

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -249,3 +249,12 @@ umask 022
 if [ -d "$HOME/bin" ] ; then
     PATH="$HOME/bin:$PATH"
 fi
+
+# enable bash completion in interactive shells
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
