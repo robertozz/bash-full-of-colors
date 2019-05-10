53c6dbcbe701d05294849aa7c0fa4cddc8ecc25e
Roberto L <robertozz@users.noreply.github.com>
2019-05-10 09:40:51 +0200

    Update bash_profile.sh

--- bash_profile.sh
+++ bash_profile.sh
@@ -13,13 +13,13 @@ fi
 
 figlet -ct -f roman $HOSTNAME
 echo ""
-echo "Qui è già impostata una versione base di Ubuntu con installato e configurato:"
+echo -e "${BYellow}Qui è già impostata una versione base di Ubuntu con installato e configurato:${Color_Off}"
 echo " - Indirizzo IP impostato da router con il MAC"
 echo " - OpenSSH"
 echo " - IPTables (https://wiki.ubuntu-it.org/Sicurezza/Iptables)"
 echo " - Ufw attivo"
 echo " - Fail2Ban"
 echo ""
-echo "Se devi utilizzarlo per un nuovo server apporta le modifiche in"
+echo -e "${BYellow}Se devi utilizzarlo per un nuovo server apporta le modifiche in${Color_Off}"
 echo " - /etc/hosname"
 echo " - le regole di IPTables e Ufw"
